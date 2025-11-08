1c1553fbae13998c1ea148e1bd32951a0cec8f5f
Akari-Codes <astilcodex6996@gmail.com>
2025-11-08 17:50:53 +0000

    jq

--- posters/morejs.js
+++
@@ -1,5 +0,0 @@
-
-function getanimecount() {var animecount = '21';return animecount;};
-function getmoviecount() {var moviecount = '0';return moviecount;};
-function getamcount() {var amcount = '0';return amcount;};
-function gettvscount() {var tvscount = '0';return tvscount;};       
