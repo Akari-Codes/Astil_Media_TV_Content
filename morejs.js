8cdcfd34adbb851be81c87cabfd27c5d113477f1
Akari-Codes <astilcodex6996@gmail.com>
2025-11-08 17:48:56 +0000

    Update anime count in getanimecount function

--- morejs.js
+++ morejs.js
@@ -1,4 +1,4 @@
 
-function getanimecount() {var animecount = '224';return animecount;};
+function getanimecount() {var animecount = '223';return animecount;};
 function getamcount() {var amcount = '48';return amcount;};
 function gettvscount() {var tvscount = '16';return tvscount;};       
