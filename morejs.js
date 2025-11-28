037a65b5e7b13a40577ce9dc57426e2ff0118d9c
Akari-Codes <astilcodex6996@gmail.com>
2025-11-28 09:26:00 +0000

    a

--- morejs.js
+++ morejs.js
@@ -1,4 +1,4 @@
 
-function getanimecount() {var animecount = '282';return animecount;};
-function getamcount() {var amcount = '53';return amcount;};
+function getanimecount() {var animecount = '338';return animecount;};
+function getamcount() {var amcount = '60';return amcount;};
 function gettvscount() {var tvscount = '16';return tvscount;};       
